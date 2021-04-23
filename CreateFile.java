60644053a79a1b43dc8c292c5e707e5919f77bc2
sonimakumari <78236013+sonimakumari@users.noreply.github.com>
2021-04-23 15:01:22 -0400

    Update CreateFile.java

--- CreateFile.java
+++ CreateFile.java
@@ -5,7 +5,7 @@ import java.io.IOException;  // Import the IOException class to handle errors
 public class CreateFile {
   public static void main(String[] args) {
     try {
-      File myObj = new File("/Users/sonimamishra/Documents/Sonima/filename.txt");
+      File myObj = new File("/Users/filename.txt");
       if (myObj.createNewFile()) {
         System.out.println("File created: " + myObj.getName());
       } else {
